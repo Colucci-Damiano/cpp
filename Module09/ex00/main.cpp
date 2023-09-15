27618f45769530bc69fb52be37efd7d708d2cb11
Damiano Colucci <dcolucci@c2r1p7.42firenze.it>
2023-09-15 18:30:25 +0200

    Module09 restart ex00

--- Module09/ex00/main.cpp
+++ Module09/ex00/main.cpp
@@ -1,41 +1,67 @@
 
 #include "BitcoinExchange.hpp"
 #include <iostream>
+#include <limits>
 #include <ctime>
+
+bool	isNumber( std::string const & str )
+{
+	size_t const	len = str.length();
+	size_t			i = 0;
+
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	if (len == 0)
+		return(false);
+	for (; i < len; i++)
+	{
+		if (!std::isdigit(str[i]))
+			return (false);
+	}
+	return (true);
+}
+
+int		atoiLimits( std::string const & str )
+{
+	long int		n = 0;
+	long int const	maxInt = std::numeric_limits<int>::max();
+	size_t			len = str.length();
+	size_t			i = 0;
+	int				sign = 1;
+
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = -1 ;
+		i++;
+	}
+	for(; i < len; i++)
+	{
+		if (std::isdigit(str[i]))
+		{
+			n = n * 10 + (str[i] - 48);
+			if (n > maxInt)
+				throw(std::runtime_error("Overflow"));
+		}
+		else
+			break ;
+	}
+	return (n * sign);
+}
+
+int		toNumber( std::string const & str )
+{
+	if (!isNumber(str))
+		throw(std::runtime_error("Bad input"));
+	return(::atoiLimits(str));
+}
 
 int	main( int ac, char **av )
 {
 	if (ac != 2)
 	{
-		std::cout << "Error, invalid arguments.\nUsage : ./btc <input_file>" << std::endl;
+		std::cerr << "Error, invalid arguments.\nUsage : ./btc <input_file>" << std::endl;
 		return (1);
 	}
-
-	try
-	{
-		BitcoinExchange		data("data.csv");
-		std::string			line;
-		time_t				date;
-
-		//data.showResults(av[1]);
-		//int	year, month, day;
-		(void)av;
-		while (!std::cin.fail())
-		{
-			std::cout << "Insert date in format YYYY-MM-DD: ";
-			std::cin >> line;
-			date = BitcoinExchange::parseDate(line);
-			std::cout << date << std::endl;
-			std::cout << std::localtime(&date)->tm_year + 1900 << std::endl;
-			std::cout << std::localtime(&date)->tm_mon + 1 << std::endl;
-			std::cout << std::localtime(&date)->tm_mday << std::endl;
-		}
-	}
-	catch(BitcoinExchange::InputFileException & e)
-	{
-		std::cout << "Exception: " << e.what() << std::endl;
-	}
-
-
 	return (0);
 }
